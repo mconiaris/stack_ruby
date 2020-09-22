0c03d40b3ad755aebc69b81c47ba7ae16d2677db
mconiaris <mconiaris@gmail.com>
2020-09-22 12:53:00 -0400

    Removed the brackets from the test.

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 RSpec.describe Hello do
 	describe Hello do
 	 it 'Hello#hello should print message' do
-	  expect {Hello.hello}.to eq('Hello world!')
+	  expect Hello.hello.to eq('Hello world!')
 	 end
 	end
 end
